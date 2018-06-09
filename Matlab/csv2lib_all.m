6760e5ffb12e20a2f8c8b41f7ac416cd0732e91c
mp-17 <twisted@live.it>
2018-06-09 15:41:20 +0200

    bigger function to convert all csv in a folder to mtx ready to liberty

---
+++ Matlab/csv2lib_all.m
@@ -0,0 +1,48 @@
+%%------------- csv2lib_all -------------
+
+% take all the csv files from a folder and create a file with all the
+% matrices ready to be put in the liberty file
+
+%------------- Parameters -------------
+
+% input directory - to end with "/"
+dirInPath = "/home/matteo/git/standard-cell-design/HA_X1/simulations/schematic/25deg/S/";
+
+% output file path
+fOutPath = "./ready2lib";
+
+% specify the Unit of Measurement for time measures (specify the value of the exponent)
+timeUoM = -9; % [nano seconds]
+
+%------------- Preliminary operations -------------
+
+% enumerate all the csv files and check there is at least one csv
+fInFolder = dir(dirInPath + "*.csv");
+
+if (isempty(fInFolder))
+    error("error : no .csv files in the specified directory");
+else
+    
+%------------- Write the output file -------------
+
+% obtain the ID of the output file
+    fOutID = fopen(fOutPath, 'w');
+    
+    % check if it's opened
+    if (fOutID ~= -1)
+        
+        % for each file, append a new matrix to the output file
+        for i = 1:size(fInFolder, 1)
+            fCurr = dirInPath + fInFolder(i).name;
+            fTag = fInFolder(i).name(1, 1:end-4);
+            csv2lib_function(fTag, fCurr, fOutID, timeUoM)
+        end
+
+        % close the output file
+        fclose(fOutID);
+
+    else
+        error("output file error : can't open it");
+    end
+
+end
---
+++ Matlab/csv2lib_function.m
@@ -0,0 +1,48 @@
+%% csv2lib_function
+
+% Convert a .csv file exported from Virtuoso_ADE-L simulation in a matrix
+% ready to be put in a liberty file.
+% destination file matrix will have in each cell the data corresponding to
+% the intersection between a precise tr/tf and a capacitance. On rows we
+% will have constant capacitances, on columns constant tr/tf.
+
+% INPUT %
+% fTag = identifier to identify the kth-matrix
+% fInPath = path of the current csv file
+% fOutID = ID of the output file
+% timeUoM = Unit of Measurement for time measures
+
+function csv2lib(fTag, fInPath, fOutID, timeUoM)
+
+%% Import data
+
+% read data from CSV file from the second row (the first contains only indexes)
+mtx_tmp = csvread(fInPath, 1, 0);
+
+%% Elaborate the matrix
+
+% delete x-axis informations
+mtx = mtx_tmp(:, 2:2:end);
+
+% convert (time) data to the right output-format (normalize to the UoM specified in the liberty file index)
+mtx_norm = mtx * 10^(-timeUoM);
+
+%% Write current matrix on the new file
+
+% specify the string and write it on the output file
+
+tagStr = fTag + "\n";
+fprintf(fOutID, tagStr);
+
+begStr = 'values ("%1.8f,%1.8f,%1.8f,%1.8f,%1.8f,%1.8f,%1.8f", \\\n';
+fprintf(fOutID, begStr, mtx_norm(1, :));
+
+formatSpec = '        "%1.8f,%1.8f,%1.8f,%1.8f,%1.8f,%1.8f,%1.8f", \\\n';
+fprintf(fOutID, formatSpec, mtx_norm(2:end-1, :));
+
+endStr = '        "%1.8f,%1.8f,%1.8f,%1.8f,%1.8f,%1.8f,%1.8f");*/';
+fprintf(fOutID, endStr, mtx_norm(end, :));
+
+% space for the new csv
+fprintf(fOutID, "\n\n");
+
