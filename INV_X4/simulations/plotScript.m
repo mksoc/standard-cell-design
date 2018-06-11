4257104f494f1b1801b24bcb4b1d0c597b1f586e
Marco <27402195+mksoc@users.noreply.github.com>
2018-06-11 21:24:12 +0200

    Added Matlab script to export figs in pdf

--- INV_X4/simulations/plotScript.m
+++ INV_X4/simulations/plotScript.m
@@ -1,3 +1,7 @@
+clearvars
+close all
+clc
+
 %% Read .csv simulation files
 t_F = csvread('t_F.csv', 1, 0);
 t_R = csvread('t_R.csv', 1, 0);
@@ -8,39 +12,39 @@ tp_H = csvread('tp_H.csv', 1, 0);
 figure(1)
 clf
 for i = 1:7
-    plot(t_F(:, (2*i-1)).*1e15, t_F(:, 2*i));
+    plot(t_F(:, (2*i-1)).*1e15, t_F(:, 2*i).*1e12);
     hold on
 end
 grid on
-title('Output 70%-30% falling time')
+ylabel('Output 70%-30% falling time [ps]')
 xlabel('Load Capacitance [fF]')
 
 figure(2)
 clf
 for i = 1:7
-    plot(t_R(:, (2*i-1)).*1e15, t_R(:, 2*i));
+    plot(t_R(:, (2*i-1)).*1e15, t_R(:, 2*i).*1e12);
     hold on
 end
 grid on
-title('Output 30%-70% rising time')
+ylabel('Output 30%-70% rising time [ps]')
 xlabel('Load Capacitance [fF]')
 
 figure(3)
 clf
 for i = 1:7
-    plot(tp_L(:, (2*i-1)).*1e15, tp_L(:, 2*i));
+    plot(tp_L(:, (2*i-1)).*1e15, tp_L(:, 2*i).*1e12);
     hold on
 end
 grid on
-title('Output falling 50%-50% propagation delay')
+ylabel('Output falling 50%-50% propagation delay [ps]')
 xlabel('Load Capacitance [fF]')
 
 figure(4)
 clf
 for i = 1:7
-    plot(tp_H(:, (2*i-1)).*1e15, tp_H(:, 2*i));
+    plot(tp_H(:, (2*i-1)).*1e15, tp_H(:, 2*i).*1e12);
     hold on
 end
 grid on
-title('Output rising 50%-50% propagation delay')
+ylabel('Output rising 50%-50% propagation delay [ps]')
 xlabel('Load Capacitance [fF]')
